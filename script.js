b676018d46b4ab9022b9f9dfcf612eb6f705e11b
Seth Greenbaum <sethmgreenbaum@gmail.com>
2020-01-10 16:54:53 -0700

    getting the framework in place

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-$(document).ready(function() {
-
+var startButton = $("#start-button");
+var questionDisplay = $("#question-display");
+var questionForm =$("#question-form");
+startButton.on("click", function () {
+    questionDisplay.attr("class", "d-none");
+    questionForm.attr("class","d-block");
 });
+
+//coutdown timer, subtract 15 seconds from wrong answers
+//timer is initiated by button
+//0 on timer ends the game
+//algorithms sections have timer
+//initials and scores got into local storage
+//view high scores button
+//time counter in corner
+//use bootstrap
+//create div, then override content with object
+//Scores can be determined as I wish
+//you can use a second html file for high score page
+//use a for loop to loop through questions instead of next button
+//flash "correct" or "incorrect"
+//use multiple javascript files
+
 
